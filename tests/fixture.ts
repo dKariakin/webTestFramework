dc1a4521b6cc9d4690ab6f0615f66cae461b458e
Dima <5418022+dKariakin@users.noreply.github.com>
2025-03-13 00:48:10 +0100

    first login tests

--- tests/fixture.ts
+++ tests/fixture.ts
@@ -1,7 +1,7 @@
-import { test } from '@playwright/test';
+import { test as base } from '@playwright/test';
 import AuthPage from '../src/pages/auth';
 
-test.extend<{ auth: AuthPage }>({
+export const test = base.extend<{ auth: AuthPage }>({
   auth: async ({ page }, use) => {
     await use(new AuthPage(page));
   },
