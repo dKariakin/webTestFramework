c02c2b8c07c6e5d3c212807636dc3fd59504939b
Dima <5418022+dKariakin@users.noreply.github.com>
2025-03-17 00:52:59 +0100

    added initial tests for signup flow

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { mockLoginSuccess } from '../src/mocks/auth';
+import { mockLoginSuccess, mockSignupSuccess } from '../src/mocks/auth';
 import { test } from './fixture';
 
 test.describe('Login flow', () => {
@@ -7,8 +7,9 @@ test.describe('Login flow', () => {
   });
 
   test('When user logs in with correct credentials, profile page should be opened', async ({ auth, profile }) => {
+    const expectedEmail = 'test@email.com';
     const randUser = {
-      email: 'test@email.com',
+      email: expectedEmail,
       pwd: '123456',
     };
     await mockLoginSuccess(auth.getPage());
@@ -16,5 +17,43 @@ test.describe('Login flow', () => {
     await auth.login(randUser.email, randUser.pwd);
 
     await profile.shouldHavePageTitle('Welcome to your profile');
+    await profile.shouldHaveUserEmail(expectedEmail);
+  });
+});
+
+test.describe('Signup flow', () => {
+  test.beforeEach(async ({ auth }) => {
+    await auth.open();
+  });
+
+  test('When a user signs up with correct data, profile page should be opened', async ({ auth, profile }) => {
+    const expectedEmail = 'new_test@email.com';
+    const newUser = {
+      email: expectedEmail,
+      pwd: '1We45678',
+    };
+    await mockSignupSuccess(auth.getPage());
+
+    await auth.signup(newUser.email, newUser.pwd);
+
+    await profile.shouldHavePageTitle('Welcome to your profile');
+    await profile.shouldHaveUserEmail(expectedEmail);
+  });
+
+  [
+    { title: 'empty password', pwd: '' },
+    { title: 'lowercase letters only', pwd: 'qweqweqwe' },
+    { title: 'no lowercase letters', pwd: '1Q345678' },
+    { title: 'letters only', pwd: 'qweQWEqwe' }
+  ].forEach((tc: { title: string, pwd: string }) => {
+    test(`When user signs up using a password that doesn't follow requirements, error message should be shown ${tc.title}`, async ({ auth }) => {
+      // Current requirements to the password: 
+      // - 8 characters, 
+      // - at least 1 number, 
+      // - at least 1 capital letter
+      await auth.signup('test@email.com', tc.pwd);
+
+      await auth.shouldHavePasswordError('Password must be at least 8 characters with at least one number and one uppercase letter');
+    });
   });
 });
